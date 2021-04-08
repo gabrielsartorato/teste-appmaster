fc34b72f8df76b2c07f23ec8557b7e91435a5062
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2021-04-08 19:28:08 -0300

    feat: add class global exception error

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,30 @@
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(process.env.APP_PORT || 3333, () => {
   console.log(`Server is running at por ${process.env.APP_PORT}`);
